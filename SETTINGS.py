11497f36d75cafc3c3d73af2bd593d96617ba585
Varyamo123 <78637154+Varyamo123@users.noreply.github.com>
2021-02-09 16:34:18 +0800

    Настройки бота

---
+++ SETTINGS.py
@@ -0,0 +1,14 @@
+URL_USD = 'https://banki24.by/minsk/kurs/usd'
+URL_EUR = 'https://banki24.by/minsk/kurs/eur'
+URL_RUB = 'https://banki24.by/minsk/kurs/rub'
+TOKEN = '1535031655:AAE6AS82rnkF0NKjaQW7qGAW9QlWOR8og7s'
+
+BUTTON_START = 'В начало'
+
+BUTTON_USD = 'USD'
+BUTTON_EUR = 'EUR'
+BUTTON_RUB = 'RUB'
+
+BUTTON_GET_ALL_LIST = 'Вывести весь список банков'
+BUTTON_BEST_BUY_VALUE = 'Вывести лучший курс для покупки'
+BUTTON_BEST_SELL_VALUE = 'Вывести лучший курс для продажи'
